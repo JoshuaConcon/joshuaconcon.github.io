0a593dca0d9567a8954d81e3eb82230f77e27362
Joshua Concon <joshua.concon@scotiabank.com>
2019-04-09 09:45:50 -0400

    Updates

---
+++ precache-manifest.034f5cea626a1f634a8898b803330b9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4462411561d0905b8d34",
+    "url": "/static/js/main.bc411b1d.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "4462411561d0905b8d34",
+    "url": "/static/css/main.d452fb0f.chunk.css"
+  },
+  {
+    "revision": "fd713e53c00cf3aeee2287031509899c",
+    "url": "/index.html"
+  }
+];
