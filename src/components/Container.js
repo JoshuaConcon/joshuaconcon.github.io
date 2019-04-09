b61b6ac4c8ee8f0a68d2fae900ec350e6d007dc8
JoshuaConcon <conconjoshua@gmail.com>
2019-04-09 11:24:24 -0400

    Refactored out Container Component

---
+++ src/components/Container.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Container.css';
+
+class Container extends Component {
+  render() {
+    return (
+      <header className="App-header">
+          test
+      </header>
+    );
+  }
+}
+
+export default Container;
