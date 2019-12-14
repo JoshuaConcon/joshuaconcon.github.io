9259afcc4ae3afd3471aacbb7b3350b2ce0778bb
JoshuaConcon <conconjoshua@gmail.com>
2019-12-14 16:40:37 -0500

    trying to add aliases

---
+++ src/aliases.js
@@ -0,0 +1,6 @@
+import moduleAlias from 'module-alias';
+import path from 'path';
+
+moduleAlias.addAlias('@components', path.join(__dirname, '/components'));
+moduleAlias.addAlias('@utils', path.join(__dirname, '/utils'));
+moduleAlias.addAlias('@sections', path.join(__dirname, '/sections'));
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import alias from './aliases';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
