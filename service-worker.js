a882eacfb8c821b243796275180b6794b604f65e
JoshuaConcon <conconjoshua@gmail.com>
2019-12-15 17:09:07 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e2c2ec3c6011286a8a68fb759b598e84.js"
+  "/precache-manifest.f877da5f73b59552155c6598adfe1c43.js"
 );
 
 workbox.clientsClaim();
